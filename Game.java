729c1eceba4813a10b74f0a7e5b05eb6a0f9acc3
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2019-06-02 12:05:54 +0200

    Commit 03 - Elminacion repeticion de cogido clase Game

--- Game.java
+++ Game.java
@@ -60,7 +60,7 @@ public class Game
         eras.setExits(aeropuerto, null, depositos, pinilla);
         depositos.setExits(eras, null, null, padreIsla);
         aeropuerto.setExits(null, null, eras, null);
-        
+
         currentRoom = santo;  // start game outside
     }
 
@@ -92,20 +92,7 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
+        lugarActual();
         System.out.println();
     }
 
@@ -187,22 +174,26 @@ public class Game
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null) {
-                System.out.print("north ");
-            }
-            if(currentRoom.eastExit != null) {
-                System.out.print("east ");
-            }
-            if(currentRoom.southExit != null) {
-                System.out.print("south ");
-            }
-            if(currentRoom.westExit != null) {
-                System.out.print("west ");
-            }
-            System.out.println();
+            lugarActual();
         }
+    }
+
+    private void lugarActual()
+    {    System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exits: ");
+        if(currentRoom.northExit != null) {
+            System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null) {
+            System.out.print("east ");
+        }
+        if(currentRoom.southExit != null) {
+            System.out.print("south ");
+        }
+        if(currentRoom.westExit != null) {
+            System.out.print("west ");
+        }
+        System.out.println();
     }
 
     /** 
